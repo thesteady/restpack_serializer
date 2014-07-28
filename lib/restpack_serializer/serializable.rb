088a29f4e63a5c768d19f19435ba9e639baa35fb
Laura Steadman <laura@quickleft.com>
2014-07-28 16:53:00 -0600

    Update has_many, through ids in links to be strings instad of numbers

--- lib/restpack_serializer/serializable.rb
+++ lib/restpack_serializer/serializable.rb
@@ -72,7 +72,7 @@ module RestPack
             data[:links][association.name.to_sym] = foreign_key_value.to_s
           end
         elsif association.macro == :has_many && association.options[:through]
-          ids = model.send(association.name).pluck(:id)
+          ids = model.send(association.name).pluck(:id).map { |id| id.to_s }
 
           data[:links] ||= {}
           data[:links][association.name.to_sym] = ids
